31b1cad1fa9dc635407105c21a8524afba2c532b
Serranoapple <peter.boegh.soerensen@gmail.com>
2026-05-16 22:14:58 +0200

    Create strava_service.py

---
+++ app/services/strava_service.py
@@ -0,0 +1,25 @@
+import os
+import requests
+
+STRAVA_CLIENT_ID = os.getenv("STRAVA_CLIENT_ID")
+STRAVA_CLIENT_SECRET = os.getenv("STRAVA_CLIENT_SECRET")
+STRAVA_REFRESH_TOKEN = os.getenv("STRAVA_REFRESH_TOKEN")
+
+
+def refresh_access_token():
+
+    response = requests.post(
+        "https://www.strava.com/oauth/token",
+        data={
+            "client_id": STRAVA_CLIENT_ID,
+            "client_secret": STRAVA_CLIENT_SECRET,
+            "grant_type": "refresh_token",
+            "refresh_token": STRAVA_REFRESH_TOKEN
+        }
+    )
+
+    data = response.json()
+
+    print("TOKEN REFRESH RESPONSE:", data)
+
+    return data["access_token"]
